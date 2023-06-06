c2cf4071f6ba4f0a9667b2d3cd7f0cd8eceb0af0
Omarhussien01 <94190124+Omarhussien01@users.noreply.github.com>
2023-06-06 12:42:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -12,12 +12,12 @@ window.addEventListener('load', () => {
 		const task = input.value;
         const priority = pr.value; 
 
-        if(task==''||priority==''){
+        if(task === ''|| priority === ''){
             alert('Task Name and priority canot be empty');
             return;
-        }else if(taskNameArray.includes(task)){
+        }else if(taskNameArray.includes(task) === true){
                 alert('Please Enter A unique Task Name');
-                return;
+				return;
         } 
         else{
             taskNameArray.push(task);
@@ -69,24 +69,37 @@ window.addEventListener('load', () => {
 
 		input.value = '';
         pr.value='';
-
+		console.log(taskNameArray);
 		task_edit_el.addEventListener('click', (e) => {
-            const taskName = document.getElementsByClassName('text');
-			if (task_edit_el.innerText.toLowerCase() == "edit") {
-				task_edit_el.innerText = "Save";
-				task_input_el.removeAttribute("readonly");
-                task_pr_el.removeAttribute('readonly');
-				task_input_el.focus();
-                task_pr_el.focus();
-			} else {
-				task_edit_el.innerText = "Edit";
-				task_input_el.setAttribute("readonly", "readonly");
+
+			taskNameArray.splice(taskNameArray.indexOf(task), 1);
+			task_edit_el.style.display = 'none';
+			const task_save_el = document.createElement('button');
+			task_save_el.classList.add('save');
+			task_save_el.innerText = 'Save';
+			task_actions_el.appendChild(task_save_el);
+			task_input_el.removeAttribute("readonly");
+			task_pr_el.removeAttribute('readonly');
+			task_input_el.focus();
+			task_pr_el.focus();
+			task_save_el.addEventListener('click', (e) => {
+				if (task_input_el.value != '' && task_pr_el.value != '' && taskNameArray.includes(task_input_el.value) === false){
+					task_save_el.style.display = 'none';
+					task_edit_el.style.display = 'inline';
+                    task_input_el.setAttribute("readonly", "readonly");
                 task_pr_el.setAttribute("readonly", "readonly");
-			}
+					taskNameArray.push(task_input_el.value);
+					console.log(taskNameArray);
+				} else {
+                    alert('please enter a valid and not repeated task name and valid priority')
+				}
+			});
+
 		});
 
 		task_delete_el.addEventListener('click', (e) => {
 			list_el.removeChild(task_el);
+			taskNameArray.splice(taskNameArray.indexOf(task), 1);
 		});
 
 	});
